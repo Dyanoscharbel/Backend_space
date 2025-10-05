71ec34afe08e5b1b75b996c330c58d24ffdf3688
SANNI Adéchinan Dyanos Charbel <135537656+Dyanoscharbel@users.noreply.github.com>
2025-10-05 14:50:18 +0100

    Add Vercel app URL to CORS origins

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 // Middlewares
 app.use(helmet());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5175', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:5175', 'http://localhost:3000', 'http://127.0.0.1:5173','nyx-a-ifront-q25a.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
